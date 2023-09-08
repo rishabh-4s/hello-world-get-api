35c20dd5c150e94e7fb1cbd42ec692865b870ffe
Rishabh Sukhija <79386635+rishabh-4s@users.noreply.github.com>
2023-09-09 04:38:35 +0530

    a

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
+app.use(cors());
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello, World!' });
+  res.json({ "message": "hello world" });
 });
 
 const PORT = process.env.PORT || 3000;
@@ -10,4 +11,4 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-module.exports = app;
+module.exports = app; 
